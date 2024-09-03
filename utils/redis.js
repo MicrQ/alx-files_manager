ff37a2d45cd975e2badbe246155b333d034e7a4a
AbenetGebre <mail.micrq@proton.me>
2024-09-03 23:37:37 +0300

    modules reinstalled. task modified

--- utils/redis.js
+++ utils/redis.js
@@ -3,19 +3,24 @@ import redis from 'redis';
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient()
+    this.client = redis.createClient();
     this.client.on('error', (err) => {
       console.log(`Redis client faild to connect: ${err}`);
     });
   }
 
-  isAlive = () => this.client.connected;
-  get = async (key) => await this.client.get(key);
-  set = async (key, value, duration) => {
-    await this.client.setex(key, duration, value);
-  };
-  del = async (key) => await this.client.del(key);
+  isAlive() {
+    return this.client.connected;
+  }
+  async get(key) {
+    return await this.client.get(key);
+  }
+  async set(key, value, duration) {
+    return await this.client.setex(key, duration, value);
+  }
+  del = async (key) => this.client.del(key);
 }
 
 
-export default new RedisClient();
+const redisClient = new RedisClient();
+export default redisClient;
