fdd60ed45a51b2cb1d61aaccaf27cf04c33b2ab1
AbenetGebre <mail.micrq@proton.me>
2024-09-04 23:15:35 +0300

    eslint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 import AppController from '../controllers/AppController';
-import app from '../server';
 
 const mapper = (app) => {
   app.get('/stats', AppController.getStats);
   app.get('/status', AppController.getStatus);
-}
+};
 
 export default mapper;
