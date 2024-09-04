4e4bc1ae4b6156ab2e33fecf0150fbab2094e2df
AbenetGebre <mail.micrq@proton.me>
2024-09-05 02:47:56 +0300

    eslint and some modifications

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -22,17 +22,16 @@ class UsersController {
   }
 
   static async getMe(req, res) {
-    const token = req.headers['x-token'] || '';
+    const token = req.headers['x-token'];
     if (!token) return res.status(401).json({ error: 'Unauthorized' });
-      
+
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
-    if (!userId)
-      return res.status(401).json({ error: 'Unauthorized' });
-    const user = await dbClient.users.findOne({ _id: userId });
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+    const user = await dbClient.users.findOne({ _id: dbClient.ObjectId(userId) });
     if (!user) return res.status(401).json({ error: 'Unauthorized' });
 
-    return res.status(200).json({ id: dbClient.ObjectId(userId), email: user.email });
+    return res.status(200).json({ id: userId, email: user.email });
   }
 }
 
