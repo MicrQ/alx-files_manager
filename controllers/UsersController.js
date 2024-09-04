317255ea825f8560bc63b777f5cb35e451763a47
AbenetGebre <mail.micrq@proton.me>
2024-09-05 01:08:28 +0300

    eslint UserControllers.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,16 +1,16 @@
-import dbClient from "../utils/db";
 import sha1 from 'sha1';
+import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
     const email = req.body ? req.body.email : null;
     const password = req.body ? req.body.password : null;
 
-    if (!email) return res.status(400).json({ error: "Missing email" });
-    if (!password) return res.status(400).json({ error: "Missing password" });
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
     const userExists = await dbClient.users.findOne({ email });
-    if (userExists) return res.status(400).json({ error: "Already exists" });
+    if (userExists) return res.status(400).json({ error: 'Already exists' });
 
     const result = await dbClient.users.insertOne({ email, password: sha1(password) });
 
